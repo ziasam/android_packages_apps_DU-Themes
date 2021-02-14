f9c1f13a39935e5cd4c26d086f1f9f01357d14d6
Ziauddin Sameer <ziauddinsameer@gmail.com>
2021-02-14 14:29:47 +0000

    DU-Themes: panelbg style [2/3]

--- src/com/dirtyunicorns/themes/Themes.java
+++ src/com/dirtyunicorns/themes/Themes.java
@@ -81,6 +81,9 @@ public class Themes extends PreferenceFragment implements ThemesListener {
     public static final String PREF_FONT_PICKER = "font_picker";
     public static final String PREF_STATUSBAR_ICONS = "statusbar_icons";
     public static final String PREF_THEME_SWITCH = "theme_switch";
+    private static final String BRIGHTNESS_SLIDER_STYLE = "brightness_slider_style";
+    private static final String PREF_PANEL_BG = "panel_bg";
+    private static final String PREF_QS_SHAPE = "qs_shape";
 
     private int mBackupLimit = 10;
     private static boolean mUseSharedPrefListener;
@@ -106,6 +109,9 @@ public class Themes extends PreferenceFragment implements ThemesListener {
     private Preference mRestoreThemes;
     private Preference mThemeSchedule;
     private CustomPreference mWpPreview;
+    private ListPreference mBrightnessSliderStyle;
+    private ListPreference mPanelBg;
+    private ListPreference mQsShape;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -314,6 +320,15 @@ public class Themes extends PreferenceFragment implements ThemesListener {
             }
         }
         mThemeSwitch.setSummary(mThemeSwitch.getEntry());
+
+        mPanelBg = (ListPreference) findPreference(PREF_PANEL_BG);
+        int mPanelValue = getOverlayPosition(ThemesUtils.PANEL_BG_STYLE);
+        if (mPanelValue != -1) {
+            mPanelBg.setValue(String.valueOf(mPanelValue + 2));
+        } else {
+            mPanelBg.setValue("1");
+        }
+        mPanelBg.setSummary(mPanelBg.getEntry());
 
         // Font picker
         mFontPicker = (ListPreference) findPreference(PREF_FONT_PICKER);
@@ -676,6 +691,21 @@ public class Themes extends PreferenceFragment implements ThemesListener {
                 }
                 mThemeSwitch.setSummary(mThemeSwitch.getEntry());
             }
+
+            if (key.equals(PREF_PANEL_BG)) {
+                String panelBg = sharedPreferences.getString(PREF_PANEL_BG, "1");
+                int panelBgValue = Integer.parseInt(panelBg);
+                  String overlayName = getOverlayName(ThemesUtils.PANEL_BG_STYLE);
+                if (overlayName != null) {
+                    handleOverlays(overlayName, false, mOverlayManager);
+                }
+                if (panelBgValue > 1) {
+                	Utils.showSystemUiRestartDialog(getContext());
+                    handleOverlays(ThemesUtils.PANEL_BG_STYLE[panelBgValue -2],
+                            true, mOverlayManager);
+                }
+            mPanelBg.setSummary(mPanelBg.getEntry());
+           }
         }
     };
 
